551c43c72df993bcc460633505f5458555836fe4
123lavanya-tiwade <lavanya.tiwade.batch2024@sitnagpur.siu.edu.in>
2026-01-20 12:05:26 +0530

    Add files via upload
    C programs of exp 2

---
+++ pr1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+	int dividend, divisor, quotient, remainder;
+	printf("Enter dividend: ");
+scanf("%d", &dividend);
+	printf("Enter divisor: ");
+	scanf("%d", &divisor);
+
+	quotient = dividend/divisor;
+	remainder = dividend % divisr
+r
+
+
+
+
+
+
+
+
+
+
+
---
+++ pr4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	int a, result;
+	printf("Enter number: ");
+	scanf("%d", &a);
+	if (a % 2 == 0) {
+	printf("Number is even\n");
+	}else{
+	printf("Number is not even\n");
+	return 0;
+}
+}
